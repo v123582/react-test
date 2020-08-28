f0f3de40910de702018684d1dbefa02c13393614
v123582 <v123582@gmail.com>
2020-08-28 12:56:05 +0800

    deploy

---
+++ precache-manifest.b0db6ddd48613ae9ce225b134fd24b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43f4b23fe4ce678e3910e54fe6f4784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffbb23825aca48d425a9",
+    "url": "/static/css/main.e7209290.chunk.css"
+  },
+  {
+    "revision": "9205f56052eb2d7a27dd",
+    "url": "/static/js/2.6ac51738.chunk.js"
+  },
+  {
+    "revision": "b1f41e6e76fe76dfa671c076fc8f72e3",
+    "url": "/static/js/2.6ac51738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbb23825aca48d425a9",
+    "url": "/static/js/main.9948a9c0.chunk.js"
+  },
+  {
+    "revision": "fea7c20314abb77617f1",
+    "url": "/static/js/runtime-main.4781a527.js"
+  }
+]);
